889378cab8a928ef5141a46bdd26144e68830bc8
Nico Bistolfi <4433590+nicobistolfi@users.noreply.github.com>
2024-09-21 13:33:04 -0700

    include version on build (#28)

--- cmd/mockthis/main.go
+++ cmd/mockthis/main.go
@@ -8,12 +8,30 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	version     = "dev"
+	commit      = "none"
+	date        = "unknown"
+	showVersion bool
+)
+
 var rootCmd = &cobra.Command{
 	Use:   "mockthis",
 	Short: "MockThis - A CLI for managing mock API endpoints",
+	Run: func(cmd *cobra.Command, args []string) {
+		if showVersion {
+			fmt.Printf("Version: %s\n", version)
+			fmt.Printf("Commit: %s\n", commit)
+			fmt.Printf("Date: %s\n", date)
+			return
+		} else {
+			_ = cmd.Help()
+		}
+	},
 }
 
 func init() {
+
 	rootCmd.AddCommand(commands.LoginCmd)
 	rootCmd.AddCommand(commands.RegisterCmd)
 	rootCmd.AddCommand(commands.CreateEndpointCmd)
@@ -21,6 +39,8 @@ func init() {
 	rootCmd.AddCommand(commands.GetEndpointCmd)
 	rootCmd.AddCommand(commands.UpdateEndpointCmd)
 	rootCmd.AddCommand(commands.DeleteEndpointCmd)
+
+	rootCmd.Flags().BoolVarP(&showVersion, "version", "v", false, "Show version information")
 }
 
 func main() {
